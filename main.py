3f3434368bb6aeacc644b1c5def73121b339623d
Aoife O'Rourke <aoife.orourke@hotmail.com>
2020-11-15 17:06:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,119 @@
+
+
+# myList = [0,1,2,3,4]
+# for num in myList:
+#   print(num)
+
+# name = "Aoife"
+# for char in name:
+#   print(char)
+# for index in range(0, len(name)):
+#   print(name[index])
+
+
+##########################
+# number = int(input("Please enter number:"))
+# while number < 10:
+#   print(number)
+#   number +=1 #w/out this number would print infinitly. Stops at 9 because we stated <10
+
+# limit = int(input("Please enter number:"))
+# counter = 1
+# total = 0
+# while counter < limit:
+#   total += counter
+#   counter +=2
+
+# print(total)
+#############################
+# number = int(input("Please enter number:"))
+# originalNumber = number
+# while number <= 10:
+#   print(number)
+#   number +=1
+
+# number = originalNumber
+# while number <= 10:
+#   print(number)
+#   number +=1
+
+######################################
+# grade = int(input("Enter your grade: "))
+# while grade <0 or grade >100:
+#   print("ERROR grade must be between 0 and 100")
+#   grade = int(input("Enter your grade: "))
+# if grade >=90:
+#    print("H1")
+# elif grade >=80:
+#    print("H2")
+# else:
+#   print("H8")
+
+######################
+# for number in range(5):
+#   print(number)
+
+# for number in range(10, 0, -1):
+#   print(number)
+
+
+# myList = [0,1,2,3,4]
+# for item in myList:
+#   print(item)
+
+# name = "01234"
+# for char in name:
+#   print(char)
+
+# name = "Aoife"
+# for index, item in enumerate(name):
+#   print("Index: ", index)
+#   print("Item ", item)
+
+##############################
+# cols = 10
+# rows = 10
+# for row in range(rows):
+#   for col in range(cols):
+#     print("*", end=" ")
+#   print()
+
+#############################
+
+
+# for time in range(1,11):
+#   for mul in range(1,11):
+#     ans = time * mul
+#     print(time, "X", mul, "=", ans)
+#   print("\n\n")
+
+#######################################
+# print("Menu")
+# print("*"*10)
+# print("1) times tables")
+# print("2) LC grades")
+# print("3) Exit")
+# option = int(input("Enter option:"))
+
+# while option !=3:
+#   if option ==1:
+#     print("1 works")
+#   elif option ==2:
+#     print("2 works")
+#   else:
+#     print("choose 1, 2,or 3")
+#   print("Menu")
+#   print("*"*10)
+#   print("1) times tables")
+#   print("2) LC grades")
+#   print("3) Exit")
+#   option = int(input("Enter option:"))
+
+#########################################
+
+# Name = input("What is your name?: ")
+# print("hello, your name is ", Name)
+# print( "Hi", Name, "nice to meet you!")
+
+personAge =int(input("What is your age? "))
+height = float(input("What is your height? "))
